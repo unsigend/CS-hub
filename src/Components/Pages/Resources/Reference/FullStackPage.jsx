25befde0a99295dc87bf9b79f72e8b0afb159dc3
qiuyixiang <qiuyixiang2003@gmail.com>
2025-07-03 18:17:39 -0500

    add draft for full stack reference page

--- src/Components/Pages/Resources/Reference/FullStackPage.jsx
+++ src/Components/Pages/Resources/Reference/FullStackPage.jsx
@@ -6,12 +6,75 @@
 
 // Import for the Reference component
 import Reference from "@/Components/UI/Reference";
+import { Title, SubTitle } from "@/Components/UI/Title";
+import Card from "@/Components/UI/Card";
+import Note from "@/Components/UI/Note";
+
+// Import for the data
+import FullStackReference from "@/data/Resources/Reference/FullStack";
+
+const Style = {
+    Paragraph: `
+    text-gray-700 leading-relaxed
+    text-base sm:text-lg mb-3
+    [&>p]:mb-4 [&>p:last-child]:mb-0
+    [&>ul]:mb-4 [&>ol]:mb-4
+    [&>h1]:mb-4 [&>h2]:mb-4 [&>h3]:mb-4
+    [&>blockquote]:mb-4 [&>pre]:mb-4
+    space-y-0
+    `,
+    Box: `
+    flex flex-col
+    `,
+    Container: `
+    flex flex-col gap-5
+    `
+}
+
+function ConstructReference({Category, title}) {
+    return (
+        <div className={Style.Box}>
+            <SubTitle title={title} level={2} />
+            <p className={Style.Paragraph}>
+                {Category.description}
+            </p>
+            {
+                Category.map((item) => (
+                    <div key={item.id}>
+                        <SubTitle title={item.name} level={3} />
+                        {
+                            item.subContent.map((subItem) => (
+                                <div className={Style.Paragraph}>
+                                    {subItem.name} : <Reference url={subItem.url} content={subItem.content} />
+                                </div>
+                            ))
+                        }
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
 
 export default function FullStackReferencePage() {
     return (
         <div>
-            <h1>Full Stack Reference Pages <Reference url="https://www.google.com" content="Google" /></h1>
-            
+            <Card content={
+                <div className={Style.Container}>
+                    <div className={Style.Box}>
+                        <Title title="Full Stack Resources Reference" />
+                        <p className={Style.Paragraph}>
+                        Here are some quick reference for full stack development. Divide into three 
+                        parts: <strong>Frontend</strong>, <strong>Backend</strong>, and <strong>Database</strong>.
+                        Each of them has a curated list of resources about frameworks, libraries, and tools.
+                        </p>
+                    </div>
+
+                    <ConstructReference Category={FullStackReference.Frontend} title="Frontend" />
+                    <ConstructReference Category={FullStackReference.Backend} title="Backend" />
+                    <ConstructReference Category={FullStackReference.Database} title="Database" />
+                </div>
+            } />
         </div>
     )
 }
---
+++ src/data/Resources/Reference/FullStack.jsx
@@ -0,0 +1,41 @@
+/**
+ * @author: Qiu Yixiang
+ * @date: 2025-07-03
+ * @description: Full Stack Reference Data for the CS-hub project
+ */
+
+
+const FullStackReference = {
+    Frontend: [
+        {
+            name: "Frontend Framework",
+            id: "frontend-framework",
+            subContent: [
+                {
+                    name: "React Library",
+                    id: "react-library",
+                    url: "https://react.dev/reference/react",
+                    content: "React Documentation",
+                },
+                {
+                    name: "Next.js Library",
+                    id: "nextjs-library",
+                    url: "https://nextjs.org/docs",
+                    content: "Next.js Documentation",
+                },
+                {
+                    name: "React Router",
+                    id: "react-router",
+                    url: "https://reactrouter.com/home",
+                    content: "React Router Documentation",
+                }
+            ]
+
+        }
+    ],
+    Backend: [],
+    Database: [],
+}
+
+
+export default FullStackReference;
