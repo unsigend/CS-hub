a103d43272d7f901490cd4af4ee7d4e0c6284f3e
qiuyixiang <qiuyixiang2003@gmail.com>
2025-07-04 17:03:25 -0500

    update data reference category

--- src/data/Resources/Reference/FullStack.jsx
+++ src/data/Resources/Reference/FullStack.jsx
@@ -5,62 +5,121 @@
  */
 
 
-const FullStackReference = {
-    Frontend: [
-        {
-            name: "Programming Language",
-            id: "programming-language",
-            subContent: [
-                {
-                    name: "HTML/CSS/JavaScript",
-                    id: "html-css-javascript",
-                    url: "https://developer.mozilla.org/en-US/",
-                    content: "HTML/CSS/JavaScript MDN Documentation",
-                },
-                {
-                    name: "JavaScript",
-                    id: "javascript",
-                    url: "https://javascript.info/",
-                    content: "Modern JavaScript Tutorial",
-                },
-                {
-                    name: "TypeScript",
-                    id: "typescript",
-                    url: "https://www.typescriptlang.org/docs/",
-                    content: "TypeScript Documentation",
-                },
-            ]
-        },
-        {
-            name: "Frontend Framework",
-            id: "frontend-framework",
-            subContent: [
-                {
-                    name: "React Library",
-                    id: "react-library",
-                    url: "https://react.dev/reference/react",
-                    content: "React Documentation",
-                },
-                {
-                    name: "Next.js Library",
-                    id: "nextjs-library",
-                    url: "https://nextjs.org/docs",
-                    content: "Next.js Documentation",
-                },
-                {
-                    name: "React Router",
-                    id: "react-router",
-                    url: "https://reactrouter.com/home",
-                    content: "React Router Documentation",
-                }
-            ]
+const FullStackReference = [
+    {
+        name: "Programming Language",
+        id: "programming-language",
+        subContent: [
+            {
+                name: "HTML/CSS/JavaScript",
+                id: "html-css-javascript-mdn",
+                url: "https://developer.mozilla.org/en-US/docs/Web",
+                content: "HTML/CSS/JavaScript MDN",
+            },
+            {
+                name: "JavaScript Tutorial",
+                id: "modern-javascript-tutorial",
+                url: "https://javascript.info/",
+                content: "Modern JavaScript Tutorial",
+            },
+            {
+                name: "TypeScript",
+                id: "typescript-tutorial",
+                url: "https://www.typescriptlang.org/docs/",
+                content: "TypeScript Documentation",
+            },
+            {
+                name: "Python",
+                id: "python-docs",
+                url: "https://docs.python.org/3/library/index.html",
+                content: "Python Documentation",
+            }
+        ]
+    },
+    {
+        name: "Frontend Framework",
+        id: "frontend-framework",
+        subContent: [
+            {
+                name: "React",
+                id: "react-docs",
+                url: "https://react.dev/reference/react",
+                content: "React Documentation",
+            },
+            {
+                name: "React Router",
+                id: "react-router-docs",
+                url: "https://reactrouter.com/home",
+                content: "React Router Documentation",
+            },
+            {
+                name: "Next.js",
+                id: "nextjs-docs",
+                url: "https://nextjs.org/docs",
+                content: "Next.js Documentation",
+            },
+            {
+                name: "Vue.js",
+                id: "vuejs-docs",
+                url: "https://vuejs.org/guide/introduction.html",
+                content: "Vue.js Documentation",
+            },
+        ]
+    },
+    {
+        name: "UI and Component Library",
+        id: "ui-and-component-library",
+        subContent: [
+            {
+                name: "Tailwind CSS",
+                id: "tailwindcss-docs",
+                url: "https://tailwindcss.com/docs/installation/using-vite",
+                content: "Tailwind CSS Documentation",
+            },
+            {
+                name: "Shadcn UI",
+                id: "shadcn-ui-docs",
+                url: "https://ui.shadcn.com/docs/installation",
+                content: "Shadcn UI Documentation",
+            },
+            {
+                name: "Ant Design",
+                id: "ant-design-docs",
+                url: "https://ant.design/components/overview",
+                content: "Ant Design Documentation",
+            },
+            {
+                name: "Material UI",
+                id: "material-ui-docs",
+                url: "https://mui.com/material-ui/getting-started/",
+                content: "Material UI Documentation",
+            },
+            {
+                name: "Bootstrap",
+                id: "bootstrap-docs",
+                url: "https://getbootstrap.com/docs/5.3/getting-started/introduction/",
+                content: "Bootstrap Documentation",
+            },
+        ]
+    },
+    {
+        name: "Icon Library",
+        id: "icon-library",
+        subContent: [
+            {
+                name: "Lucide Icons",
+                id: "lucide-icons-docs",
+                url: "https://lucide.dev/icons/lucide",
+                content: "Lucide Icons Documentation",
+            },
+            {
+                name: "Hero Icons",
+                id: "hero-icons-docs",
+                url: "https://heroicons.com/",
+                content: "Hero Icons Documentation",
+            },
+        ]
+    }
+]
 
-        },
-        
-    ],
-    Backend: [],
-    Database: [],
-}
-
-
-export default FullStackReference;
+export default FullStackReference
