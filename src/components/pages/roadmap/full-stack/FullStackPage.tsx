9925947fb77969c8d8962866cc8c27b3b7cd2837
qiuyixiang <qiuyixiang2003@gmail.com>
2025-07-18 16:58:57 -0500

    destruct the full stack page to different sub-page

---
+++ src/components/pages/roadmap/full-stack/FullStackPage.tsx
@@ -0,0 +1,141 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2025 Qiu Yixiang
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+// Import for the components
+import Card from "@/components/ui/Card";
+import { Title, SubTitle } from "@/components/ui/Title";
+import Paragraph from "@/components/ui/Paragraph";
+import { LinkAligned } from "@/components/ui/Link";
+import UnorderedList from "@/components/ui/UnorderedList";
+import RoadMapCard from "@/components/ui/RoadMapCard";
+import { Monitor, Server, Database } from "lucide-react";
+
+// Import for the data
+import { RoadMapPath } from "@/data/content/category/categories";
+
+// Import for images
+import FULLSTACKOPEN_IMAGE from "/images/fullstackopen.svg";
+
+const FullStackContent = (): React.ReactNode => {
+  return (
+    <div>
+      <Title>Full Stack Development Road Map</Title>
+      <div className="flex flex-row gap-8">
+        <div className="flex flex-col">
+          <Paragraph>
+            This page is a road map for full stack development. This is a skill
+            and practice focused road map. If you want to learn more concept
+            based way can use the university courses for full stack development.
+          </Paragraph>
+          <SubTitle level={3}>Reference</SubTitle>
+          <LinkAligned
+            name="Full Stack Road Map"
+            href="https://roadmap.sh/full-stack"
+          >
+            Roadmap.sh Full Stack Road Map
+          </LinkAligned>
+          <LinkAligned
+            name="Backend Road Map"
+            href="https://roadmap.sh/backend"
+          >
+            Roadmap.sh Backend Road Map
+          </LinkAligned>
+          <LinkAligned
+            name="Frontend Road Map"
+            href="https://roadmap.sh/frontend"
+          >
+            Roadmap.sh Frontend Road Map
+          </LinkAligned>
+
+          <SubTitle level={3}>Note</SubTitle>
+          <Paragraph>
+            This road map has some personal bias and may not suitable for
+            everyone. Consider this as a road map reference. If you have any
+            suggestion or feedback, please feel free to contact me.
+          </Paragraph>
+          <Paragraph>
+            This page is a work in progress. I will update it as I learn more
+            about full stack development.
+          </Paragraph>
+
+          <SubTitle level={3}>Structure</SubTitle>
+          <Paragraph>
+            This road map is structured in a way that is easy to follow and
+            understand. The structure is as follows:
+          </Paragraph>
+          <UnorderedList>
+            <li>Frontend Development</li>
+            <li>Backend Development</li>
+            <li>Database</li>
+          </UnorderedList>
+          <Paragraph>
+            If you want to focused on specific technique stack, you can just
+            skip the other part. And focus on which part you want.
+          </Paragraph>
+        </div>
+        <div className="flex justify-center items-center">
+          <img
+            src={FULLSTACKOPEN_IMAGE}
+            alt="Full Stack Development"
+            className="w-full h-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const RoadMapCardBox = (): React.ReactNode => {
+  return (
+    <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 md:gap-8">
+      <RoadMapCard
+        title="Frontend"
+        icon={Monitor}
+        to={`${RoadMapPath}/full-stack/frontend`}
+      />
+      <RoadMapCard
+        title="Backend"
+        icon={Server}
+        to={`${RoadMapPath}/full-stack/backend`}
+      />
+      <RoadMapCard
+        title="Database"
+        icon={Database}
+        to={`${RoadMapPath}/full-stack/database`}
+      />
+    </div>
+  );
+};
+const FullStackPage = (): React.ReactNode => {
+  return (
+    <div className="flex flex-col gap-10">
+      <Card>
+        <FullStackContent />
+        <RoadMapCardBox />
+      </Card>
+    </div>
+  );
+};
+
+export default FullStackPage;
